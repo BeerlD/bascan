1a6c186677c382d0b2b70cc96b542a01331b3de3
BeerlD <bellkunmoreanime@gmail.com>
2025-05-23 05:47:25 -0300

    (updated) add cache file and perfom result

--- tools/nmap.sh
+++ tools/nmap.sh
@@ -3,8 +3,32 @@ source ././modules/cache.sh
 source ././modules/utils.sh
 
 function nmap_fragment() {
-    nmap -f "$1" &
+    cache_tools_file_create "nmap" "fragments_packets.txt"
+    cache_file_path=$(cache_tools_file_getPath "nmap" "fragments_packets.txt")
+
+    nmap -v -f "$1" >> "$cache_file_path" &
     utils_message_loading_pid $! "\t${YELLOW}Fragments packets${NC}..."
+    
+    function performResult() {
+        # $1 -> host
+        # $2 -> cache file path
+
+        while read line; do
+            if [[ "$line" =~ try\ -Pn ]]; then
+                echo -e " ${RED}Fail${NC}."
+                nmap -Pn -v -p- -f "$1" >> "$2" &
+                utils_message_loading_pid $! "\t${YELLOW}Fragments packets${NC} (without verification ping)..."
+                return 1
+            fi
+        done < <(cat "$2")
+
+        return 0
+    }
+
+    while performResult $1 $cache_file_path; do
+        cache_tools_file_create "nmap" "fragments_packets.txt"
+    done
+
     echo -e " ${GREEN}Done${NC}."
 }
 
