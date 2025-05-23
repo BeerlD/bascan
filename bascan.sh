04278b797bd9284163889cc90f9e4447e376fd5c
BeerlD <bellkunmoreanime@gmail.com>
2025-05-23 18:23:28 -0300

    (changes) check > scan; + end line

--- bascan.sh
+++ bascan.sh
@@ -40,7 +40,6 @@ history -r
 while true; do
     read -e -p "$(echo -e "${BLUE}>${NC} ")" userInput
     lowerUserInput="${userInput,,}"
-    echo ""
 
     if [[ "${#userInput}" -eq 0 ]]; then
         continue
@@ -56,22 +55,24 @@ while true; do
         continue
     fi
 
-    if [[ "$lowerUserInput" == "check" || "$lowerUserInput" =~ ^check\  ]]; then
-        if [[ "${#userInput}" -ge 6 ]]; then
-            if [[ "${lowerUserInput:6}" == "ports" ]]; then
+    echo ""
+
+    if [[ "$lowerUserInput" == "scan" || "$lowerUserInput" =~ ^scan\  ]]; then
+        if [[ "${#userInput}" -ge 5 ]]; then
+            if [[ "${lowerUserInput:5}" == "ports" ]]; then
                 nmap_start_scan "$1"
                 continue
             fi
         fi
 
-        if [[ "$lowerUserInput" != "check" ]]; then
-           echo -e "${RED}ERROR${NC} Invalid check operation: '${userInput:6}'."
+        if [[ "$lowerUserInput" != "scan" ]]; then
+           echo -e "${RED}ERROR${NC} Invalid scan operation: '${userInput:5}'."
         fi
         
-        echo -e "\nUsage: check <operation>"
-        echo -e "Operations:"
-        echo -e "\tports - check ports vulnerabilities"
-        echo -e "\tsubdomains - check subdomains vulnerabilities"
+        echo -e "\nUsage: scan <operation>"
+        echo -e "  Operations:"
+        echo -e "    ports - scan ports vulnerabilities"
+        echo -e "    subdomains - scan subdomains vulnerabilities"
         echo -e ""
         continue
     fi
@@ -79,7 +80,7 @@ while true; do
     if [[ "$lowerUserInput" == "install" || "$lowerUserInput" =~ ^install\  ]]; then
         if [[ "${#userInput}" -ge 8 ]]; then
             if [[ "${lowerUserInput:8}" == "all" ]]; then
-                packages_to_install=("toilet" "nmap" "python3-httpx" "nikto" "kighjas723a")
+                packages_to_install=("toilet" "nmap" "python3-httpx" "nikto")
 
                 for package in "${packages_to_install[@]}"; do
                     echo -ne "${YELLOW}[+]${NC} Installing package: ${CYAN}$package${NC}... "
@@ -90,7 +91,8 @@ while true; do
                         echo -e "${GREEN}Done${NC}."
                     fi
                 done
-
+                
+                echo ""
                 sleep 3
                 continue
             fi
@@ -100,11 +102,76 @@ while true; do
            echo -e "${RED}ERROR${NC} Invalid package: '${userInput:8}'."
         fi
 
-        echo -e "\nUsage: install <package>"
-        echo -e "Packages:"
-        echo -e "\tall - install all packages"
-        echo -e "\tnmap - install port scanner"
-        echo -e ""
+        echo "\nUsage: install <package>"
+        echo "  Packages:"
+        echo "    all - install all packages"
+        echo "    nmap - install port scanner"
+        echo ""
+        continue
+    fi
+
+    if [[ "$lowerUserInput" == "option" || "$lowerUserInput" =~ ^option\  ]]; then
+        if [[ "${#userInput}" -ge 7 ]]; then
+            if [[ "${lowerUserInput:7}" =~ ^set ]]; then
+                if [[ "${#userInput}" -ge 11 ]]; then
+                    option="${lowerUserInput:11}"
+
+                    if [[ "$option" =~ ^intensity ]]; then
+                        if [[ "$option" != "intensity" ]]; then
+                            value="${option:10}" 
+                            values=("slowly" "low" "middle" "normal" "aggressive" "insane")
+                            match=false
+
+                            for v in "${values[@]}"; do
+                                if [[ "$value" == "$v" ]]; then
+                                    match=true
+                                    break
+                                fi
+                            done
+
+                            if [[ "$match" == false ]]; then
+                                echo -e "${RED}ERROR${NC} Invalid option value: '$value'."
+                            else
+                                cache_config_file_setValue 2 "$value"
+                                echo -e "${GRREN}SETTED${NC} option intensity setted to: '$value'."
+                                echo ""
+                                continue
+                            fi
+                        fi
+                    else
+                        echo -e "${RED}ERROR${NC} Invalid option: '$option'."
+                        echo ""
+                        continue
+                    fi
+                fi
+
+                echo "\nUsage: option set <option> <value>"
+                echo "Options:"
+                echo "  intensity"
+                echo "    * slowly - Sends packets extremely slowly, useful for avoiding detection by IDS/IPS." # -T0
+                echo "    * low - A little faster than slowly, but still very discreet to avoid security alarms." # -T1
+                echo "    * middle - Reduces bandwidth and CPU usage, useful for congested networks." # -T2
+                echo "    * normal - Balanced speed and discretion (default, recommended)." # -T3
+                echo "    * aggressive - Speeds up scanning, ideal for fast networks with no security restrictions." # -T4
+                echo "    * insane - Maximum speed, can overload the network and be easily detected by firewalls." # -T5
+                echo ""
+                continue
+            fi
+
+            if [[ "${lowerUserInput:7}" == "show" ]]; then
+                continue
+            fi
+
+            echo -e "${RED}ERROR${NC} Invalid option operation: '${userInput:7}'."
+            echo ""
+            continue
+        fi
+
+        echo "\nUsage: option <operation> [...]"
+        echo "  Operations:"
+        echo "    set <option> <value> - set an option value"
+        echo "    show - show options value"
+        echo ""
         continue
     fi
 
