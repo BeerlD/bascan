f520a352c9e048b022c363121c0dff2bfb744cff
BeerlD <bellkunmoreanime@gmail.com>
2025-06-04 19:09:26 -0300

    Update install script

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ YELLOW='\033[1;33m'
 RED='\033[1;31m'
 NC='\033[0m'
 
-echo -e "${YELLOW}[+]${NC} Installing Bascan..."
+echo -e "${YELLOW}[+]${NC} Installing bascan..."
 
 if [ "$(id -u)" -ne 0 ]; then
     echo -e "${RED}[-]${NC} Error: This script must be run as root (use sudo)."
@@ -22,32 +22,30 @@ API_URL="https://api.github.com/repos/BeerlD/bascan/releases/latest"
 TARBALL_URL=$(curl -s "$API_URL" | jq -r '.tarball_url')
 
 if [ -z "$TARBALL_URL" ] || [ "$TARBALL_URL" == "null" ]; then
-    echo -e "${RED}[-]${NC} Error: Could not retrieve the tarball URL from GitHub."
+    echo -e "${RED}[-]${NC} Error: Could not retrieve the URL, try again in a few minutes."
     exit 1
 fi
 
 TMP_DIR=$(mktemp -d)
 cd "$TMP_DIR" || exit 1
 
-echo -e "${YELLOW}[+]${NC} Downloading latest release tarball..."
+echo -e "${YELLOW}[+]${NC} Downloading latest release..."
 curl -sL "$TARBALL_URL" -o source.tar.gz
 
 if [ ! -f source.tar.gz ]; then
-    echo -e "${RED}[-]${NC} Error: Failed to download the tarball."
+    echo -e "${RED}[-]${NC} Error: Failed to download, try again in few minutes."
     exit 1
 fi
 
 INSTALL_DIR="/usr/local/bin/bascan"
 rm -rf "$INSTALL_DIR"
 
-echo -e "${YELLOW}[+]${NC} Extracting source to $INSTALL_DIR..."
+echo -e "${YELLOW}[+]${NC} Extracting to $INSTALL_DIR..."
 mkdir -p "$INSTALL_DIR"
 tar -xzf source.tar.gz --strip-components=1 -C "$INSTALL_DIR"
 
-if [[ ":$PATH:" != *":/usr/local/bin:"* ]]; then
-    echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
-    export PATH=$PATH:/usr/local/bin
-fi
+chmod +x "$INSTALL_DIR/bascan.sh"
+ln -sf "$INSTALL_DIR/bascan.sh" /usr/local/bin/bascan
 
 echo -e "${YELLOW}[+]${NC} Bascan has been successfully installed to $INSTALL_DIR."
 rm -rf "$TMP_DIR"
