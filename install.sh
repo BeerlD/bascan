9e146fc8d0361624f64ed238e26245ed701edf04
BeerlD <bellkunmoreanime@gmail.com>
2025-06-04 07:05:34 -0300

    Update install

--- install.sh
+++ install.sh
@@ -4,22 +4,27 @@ YELLOW='\033[1;33m'
 RED='\033[1;31m'
 NC='\033[0m'
 
-echo -e "${YELLOW}[+]${NC} Installing bascan..."
+echo -e "${YELLOW}[+]${NC} Installing Bascan..."
 
 BIN="bascan-linux_x64"
-# URL=$(curl -s https://api.github.com/repos/BeerlD/Bascan/releases/latest | grep "browser_download_url.*$BIN" | cut -d '"' -f 4)
 URL=$(curl -s https://api.github.com/repos/BeerlD/Bascan/releases/latest | jq -r ".assets[] | select(.name==\"$BIN\") | .browser_download_url")
 
 if [ -z "$URL" ]; then
-    echo -e "${RED}[-]${NC} Error: try again in a few minutes."
+    echo -e "${RED}[-]${NC} Error: Could not retrieve the file URL. Please try again later."
     exit 1
 fi
 
-wget -O /usr/local/bin/$BIN "$URL" &> /dev/null
-chmod +x /usr/local/bin/$BIN
+echo -e "${YELLOW}[+]${NC} Downloading $BIN..."
+wget -O "/usr/local/bin/$BIN" "$URL" &> /dev/null
+
+if [ ! -f "/usr/local/bin/$BIN" ]; then
+    echo -e "${RED}[-]${NC} Error: Download failed. Check your connection or try again later."
+    exit 1
+fi
+
+chmod +x "/usr/local/bin/$BIN"
 
 export PATH=$PATH:/usr/local/bin
 echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
-source ~/.bashrc
 
-echo -e "${YELLOW}[+]${NC} $BIN has been installed (/usr/local/bin/)."
+echo -e "${YELLOW}[+]${NC} $BIN has been successfully installed (/usr/local/bin/)."
