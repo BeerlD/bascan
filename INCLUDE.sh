b313f5167ecbe12dd9e98c79d5525273dfa2fd89
BeerlD <bellkunmoreanime@gmail.com>
2025-06-03 21:31:05 -0300

    vulnerability results save

--- INCLUDE.sh
+++ INCLUDE.sh
@@ -4,6 +4,21 @@ source ././modules/utils.sh
 
 export HOST=$1
 export ports_scanned=()
+export vulnerabilities=()
+export vulnerabilities_level=()
 
 declare -g ports_scanned
+declare -g vulnerabilities
+declare -g vulnerabilities_level
 declare -g HOST
+
+function addVulnerability() {
+    # $1 -> message
+    # $2 -> level
+    #   0 -> warning
+    #   1 -> error
+    #   2 -> severe vulnerability
+
+    vulnerabilities+=("$1")
+    vulnerabilities_level+=("$2")
+}
